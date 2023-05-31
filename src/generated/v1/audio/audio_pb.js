9ba790ac544a15e4cd01833b22763ec27b5c77ce
Bryan McGrane <bmcgrane@sensory.com>
2023-05-31 09:25:19 -0600

    recompile protofiles

--- src/generated/v1/audio/audio_pb.js
+++ src/generated/v1/audio/audio_pb.js
@@ -13,13 +13,7 @@
 
 var jspb = require('google-protobuf');
 var goog = jspb;
-var global =
-    (typeof globalThis !== 'undefined' && globalThis) ||
-    (typeof window !== 'undefined' && window) ||
-    (typeof global !== 'undefined' && global) ||
-    (typeof self !== 'undefined' && self) ||
-    (function () { return this; }).call(null) ||
-    Function('return this')();
+var global = (function() { return this || window || global || self || Function('return this')(); }).call(null);
 
 var validate_validate_pb = require('../../validate/validate_pb.js');
 goog.object.extend(proto, validate_validate_pb);
